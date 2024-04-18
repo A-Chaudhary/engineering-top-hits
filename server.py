c59d1a2609261524ab2cc6cea47ec09e4f8543fd
Rachel Michelson <rachelmichelson@Rachels-Air-6.lan>
2024-04-17 20:05:41 -0400

    there is issue with displaying values to results but the create.html is looking good and working

--- server.py
+++ server.py
@@ -1,11 +1,25 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/create')
+def create():
+    return render_template('create.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+        energy_value = request.form['energyValue']
+        speechiness_value = request.form['speechinessValue']
+        acousticness_value = request.form['acousticnessValue']
+        instrumentalness_value = request.form['instrumentalnessValue']
+        liveness_value = request.form['livenessValue']
+        danceability_value = request.form['danceabilityValue']
+        
+        return render_template('results.html', energy=energy_value, speechiness=speechiness_value, acousticness=acousticness_value, instrumentalness=instrumentalness_value,liveness=liveness_value,danceability=danceability_value)
 
 if __name__ == '__main__':
     app.run(debug=True)
