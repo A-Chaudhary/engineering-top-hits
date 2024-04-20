ee2c651a9ce567c69e3fc13afd31a9de8855c9e4
Rachel Michelson <rachelmichelson@Rachels-Air-6.lan>
2024-04-20 01:01:55 -0400

    calculates top 5 similar songs and displays on /results'

--- server.py
+++ server.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template, request, redirect, url_for
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
+
+
+# reading in data
+try:
+    df = pd.read_csv("top_10000_1960-now.csv")
+except Exception as e:
+    print("Error:", e)
+
+df['Release Date'] = pd.to_datetime(df['Album Release Date'], errors='coerce')
+df_60s = df[(df['Album Release Date'] >= '1960-01-01') & (df['Album Release Date'] <= '1969-12-31')]
+df_70s = df[(df['Album Release Date'] >= '1970-01-01') & (df['Album Release Date'] <= '1979-12-31')]
+df_80s = df[(df['Album Release Date'] >= '1980-01-01') & (df['Album Release Date'] <= '1989-12-31')]
+df_90s = df[(df['Album Release Date'] >= '1990-01-01') & (df['Album Release Date'] <= '1999-12-31')]
+df_00s = df[(df['Album Release Date'] >= '2000-01-01') & (df['Album Release Date'] <= '2009-12-31')]
+df_10s = df[(df['Album Release Date'] >= '2010-01-01') & (df['Album Release Date'] <= '2019-12-31')]
+df_20s = df[(df['Album Release Date'] >= '2020-01-01') & (df['Album Release Date'] <= '2029-12-31')]
 
 @app.route('/')
 def index():
@@ -12,14 +30,40 @@ def create():
 
 @app.route('/results', methods=['POST'])
 def results():
-        energy_value = request.form['energyValue']
-        speechiness_value = request.form['speechinessValue']
-        acousticness_value = request.form['acousticnessValue']
-        instrumentalness_value = request.form['instrumentalnessValue']
-        liveness_value = request.form['livenessValue']
-        danceability_value = request.form['danceabilityValue']
-        
-        return render_template('results.html', energy=energy_value, speechiness=speechiness_value, acousticness=acousticness_value, instrumentalness=instrumentalness_value,liveness=liveness_value,danceability=danceability_value)
+    if request.method == 'POST':
+        # function to calculate Euclidean distance
+        def euclidean_distance(x1, x2):
+            return np.sqrt(np.sum((x1 - x2) ** 2))
+
+        # function to find closest songs
+        def find_closest_songs(user_input, dataset, num_songs=5):
+            distances = dataset.apply(lambda row: euclidean_distance(user_input, row), axis=1)
+            closest_songs = distances.sort_values().head(num_songs)
+            return closest_songs.index.tolist()
+
+        # user input
+        user_input = pd.Series({
+            "Instrumentalness": int(request.form['instrumentalness'])*100,
+            "Speechiness": int(request.form['speechiness'])*100,
+            "Acousticness": int(request.form['acousticness'])*100,
+            "Liveness": int(request.form['liveness'])*100,
+            "Danceability": int(request.form['danceability'])*100,
+            "Energy": int(request.form['energy'])*100
+        })
+
+        # find closest songs
+        closest_song_indices = find_closest_songs(user_input, df_20s)
+        song_names = []
+        popularities = []
+        artists = []
+        for idx in closest_song_indices:
+            song_names.append(df.loc[idx, "Track Name"])
+            popularities.append(df.loc[idx, 'Popularity'])
+            artists.append(df.loc[idx, "Artist Name(s)"])
+
+        data = zip(popularities, song_names, artists)
+
+        return render_template('results.html', data = data)
 
 if __name__ == '__main__':
     app.run(debug=True)
